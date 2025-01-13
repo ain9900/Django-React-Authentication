8b5ef428e43c31c8d24d16de65db32c810db060f
Md. Ariful Islam <ain.mail00@gmail.com>
2025-01-14 03:46:23 +0600

    Credential validation navigating to homepage and Token storing into loca storage Completed.....Next work is Protecting Routes

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -3,11 +3,36 @@ import {Box} from '@mui/material'
 import MyTextField from './forms/MyTextField'
 import MyPasswordField from './forms/MyPasswordField'
 import MyButton from './forms/MyButton'
-import {Link} from 'react-router-dom'
+import {Link, useNavigate} from 'react-router-dom'
+import { useForm } from 'react-hook-form'
+import AxiosInstance from './AxiosInstance'
+
 
 const Login = () =>{
+
+        const {handleSubmit, control} =useForm()
+        const navigate = useNavigate();
+
+        const submission = (data) => {                
+                AxiosInstance.post('login/', {        
+                  email: data.email,
+                  password: data.password,
+                })
+                .then((response) => {
+                        //console.log(response.data)
+                        localStorage.setItem('Token', response.data.token)
+                        navigate('/home');
+                })
+                .catch((error) => {
+                  console.error("Error During Login", error);
+                });
+              };
+
+
     return (
+
         <div className={"myBackground"}>
+        <form onSubmit={handleSubmit(submission)}>
             <Box className={"whiteBox"}>
 
                 <Box className={"itemBox"}>
@@ -19,19 +44,25 @@ const Login = () =>{
                 <Box className={"itemBox"}>
                         <MyTextField 
                         label={"Enter Email"}
+                        name={"email"}
+                        control={control}
 
                         />
                 </Box>
 
                 <Box className={"itemBox"}>
                         <MyPasswordField 
-                        label={"Enter Password"}                                
+                        label={"Enter Password"} 
+                        name={"password"}   
+                        control={control}                               
                         />
                 </Box>
 
                 <Box className={"itemBox"}>
                         <MyButton
                                 label={"Login"}
+                                type ={"submit"}
+                                
                         />
                 </Box>
 
@@ -40,6 +71,7 @@ const Login = () =>{
                 </Box>
 
             </Box>
+            </form>
         </div>
     )
 }
